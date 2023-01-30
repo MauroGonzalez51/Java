3e212703a84ef06e22db92338e56cd5b0956b502
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-01-30 14:17:45 +0000

    Testing the scanner class to input

--- Code/RandomStuff/Test/App.java
+++ Code/RandomStuff/Test/App.java
@@ -1,7 +1,24 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String args[]) {
-        
+    public static void main(String[] args) throws Exception {
+        // * Testing the scanner stuff, this way is "easier" to input values
+
+        // This is the way to setup the scanner class > Yep, it's a class
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un numero: ");
+
+        // * There's a way to enter every type of data
+        Integer number = sc.nextInt();
+
+        // * scanner.nextLine() > String's
+        // * scanner.nextInt() > Integers
+        // * scanner.next().charAt(0) > Char
+        // * scanner.nextDouble() > Double
+        // * scanner.nextLong() > Long
+
+        System.out.println(number);
     }
 }
