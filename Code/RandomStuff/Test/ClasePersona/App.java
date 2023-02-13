485e8ad475c6ead75947c8b5e0768035ea799455
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-02-13 13:57:49 +0000

    Adding the constructor method

--- Code/RandomStuff/Test/ClasePersona/App.java
+++ Code/RandomStuff/Test/ClasePersona/App.java
@@ -1,10 +1,10 @@
 package Code.RandomStuff.Test.ClasePersona;
 
-import Code.RandomStuff.Test.ClasePersona.Persona;
+// import Code.RandomStuff.Test.ClasePersona.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Persona persona = new Persona();
-        persona.saludar();
+        Persona persona = new Persona("Mauro", "Gonzalez", 20, 73265835);
+        System.out.format("Nombre Persona I: %s %n", persona.getNombre());
     }
 }
--- Code/RandomStuff/Test/ClasePersona/Persona.java
+++ Code/RandomStuff/Test/ClasePersona/Persona.java
@@ -4,7 +4,12 @@ public class Persona {
     private String nombre, apellido;
     private Integer edad, telefono;
 
-
+    public Persona(String nombre, String apellido, Integer edad, Integer telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
 
     public void saludar() { System.out.format("Hola, %s %n", this.nombre); }
     public void despedirse() { 
@@ -12,4 +17,9 @@ public class Persona {
         if (this.edad >= 18) System.out.println("Soy mayor de edad");
         else System.out.println("Soy menor de edad");
     }
+
+    public String getNombre() { return this.nombre; }
+    public String getApellido() { return this.apellido; }
+    public Integer getEdad() { return this.edad; }
+    public Integer getTelefono() { return this.telefono; }
 }
