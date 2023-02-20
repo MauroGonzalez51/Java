d99953566013ba08cf5d5075621f4fd886541780
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-02-20 14:02:06 +0000

    Clase cuenta casi terminada, falta agregar un bucle para que se repita el programa hasta que el usuario lo determine

--- Code/RandomStuff/Test/ClaseCuenta/App.java
+++ Code/RandomStuff/Test/ClaseCuenta/App.java
@@ -1,7 +1,25 @@
 package Code.RandomStuff.Test.ClaseCuenta;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String userOption = "";
+
+        System.out.println("Clase cuenta");
+
+        System.out.print("Desea ingresar el saldo? (Si/No): ");
+        if (scanner.hasNext()) userOption = scanner.next();
+
+        Boolean askBalance = (userOption.equalsIgnoreCase("Si")) ? true : false;
+
+        Cuenta userAccount = new Cuenta(askBalance);
+
+        System.out.println(userAccount.getBalance());
+
+        userAccount.accountDashboard();
         
+        scanner.close();
     }
 }
