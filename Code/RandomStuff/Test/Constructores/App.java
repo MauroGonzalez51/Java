3f490933de33aaf1eea2d65520a3446361b5be09
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-02-21 11:19:42 -0500

    Testing a new constructor on input

--- Code/RandomStuff/Test/Constructores/App.java
+++ Code/RandomStuff/Test/Constructores/App.java
@@ -7,5 +7,8 @@ public class App {
 
         Date date_2 = new Date(13, 12, 2004);
         System.out.format("Date format: %s", date_2.dateFormat());
+
+        Date date_3 = new Date("13/12/2004");
+        System.out.format("Date3.day: %d", date_3.getDay());
     }
 }
