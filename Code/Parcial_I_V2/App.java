515c28259251940e2030e601747503d6f825f2f5
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-02-22 16:26:08 +0000

    Constructor de la clase fecha funcional owo

--- Code/Parcial_I_V2/App.java
+++ Code/Parcial_I_V2/App.java
@@ -2,6 +2,7 @@ package Code.Parcial_I_V2;
 
 public class App {
     public static void main(String[] args) {
-        Empleado empleado = new Empleado();
+        // Empleado empleado = new Empleado();
+        // Fecha fecha = new Fecha();
     }
 }
---
+++ Code/Parcial_I_V2/Fecha.java
@@ -0,0 +1,46 @@
+package Code.Parcial_I_V2;
+
+import java.util.Scanner;
+
+public class Fecha {
+    private Integer day, month, year;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public Fecha(Integer day, Integer month, Integer year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public Fecha() {
+        System.out.println();
+
+        do {
+            System.out.format("Ingrese el dia: ");
+            this.day = (this.scanner.hasNextInt()) ? this.scanner.nextInt() : -1;
+        } while (this.day <= 0);
+
+        do {
+            System.out.format("Ingrese el mes: ");
+            this.month = (this.scanner.hasNextInt()) ? this.scanner.nextInt() : -1;
+        } while (this.month <= 0);
+
+        do {
+            System.out.format("Ingrese el año: ");
+            this.year = (this.scanner.hasNextInt()) ? this.scanner.nextInt() : -1;
+        } while (this.year <= 0);
+    }
+
+    // ! ---------- Getters ----------|>
+    public Integer getDay() { return this.day; }
+    public Integer getMonth() { return this.month; }
+    public Integer getYear() { return this.year; }
+
+    // ! ---------- Setters ----------|>
+    public void setDay(Integer day) { this.day = day; }
+    public void setMonth(Integer month) { this.month = month; }
+    public void setYear(Integer year) { this.year = year; }
+
+    public String dateFormat() { String temp = String.format("%d/%d/%d%n", this.day, this.month, this.year); return temp; }
+}
