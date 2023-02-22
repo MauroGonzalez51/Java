d8ba3ca8c9debfb93659cee76ab423241645f38c
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-02-22 15:48:37 +0000

    Cambiando en nombre pk se buguea el package con los espacios

---
+++ Code/Parcial_I_V1/Empleado.java
@@ -0,0 +1,52 @@
+package Code.Parcial_I_V1;
+
+import java.util.Scanner;
+
+public class Empleado {
+    private String nombreEmpleado;
+    private Integer horasTrabajadas;
+    private Integer edad;
+    private Double salarioBase;
+    private Double sueldoBase;
+    public Scanner scanner = new Scanner(System.in);
+
+    public Empleado(String nombreEmpleado, Integer horasTrabajadas, Integer edad, Double salarioBase) {
+        super();
+        this.nombreEmpleado = nombreEmpleado;
+        this.horasTrabajadas = horasTrabajadas;
+        this.edad = edad;
+        this.salarioBase = salarioBase;
+        this.calcularSalarioBase();
+    }
+
+    public Empleado() {
+        System.out.format("Ingrese el nombre del empleado: ");
+        if (this.scanner.hasNext()) this.nombreEmpleado = this.scanner.next();
+    
+        System.out.format("Ingrese la cantidad de horas trabajadas: ");
+        if (this.scanner.hasNextInt()) this.horasTrabajadas = this.scanner.nextInt();
+    
+        System.out.format("Ingrese la edad del empleado: ");
+        if (this.scanner.hasNextInt()) this.edad = this.scanner.nextInt();
+    
+        System.out.format("Ingrese el salario base del empleado: ");
+        if (this.scanner.hasNextDouble()) this.salarioBase = this.scanner.nextDouble();
+    
+        this.calcularSalarioBase();
+    }
+
+    public void calcularSalarioBase() { this.sueldoBase = this.salarioBase * this.horasTrabajadas; }
+
+    //! --------- Setters ------------->
+    public void setNombreEmpleado(String nombreEmpleado) { this.nombreEmpleado = nombreEmpleado; }
+    public void setHorasTrabajadas(Integer horasTrabajadas) { this.horasTrabajadas = horasTrabajadas; }
+    public void setEdad(Integer edad) { this.edad = edad; }
+    public void setSalarioBase(Double salarioBase) { this.salarioBase = salarioBase; }
+
+    // ! --------- Getters ------------->
+    public String getNombreEmpleado() { return this.nombreEmpleado; }
+    public Integer getHorasTrabajadas() { return this.horasTrabajadas; }
+    public Integer getEdad() { return this.edad; }
+    public Double getSalarioBase() { return this.salarioBase; }
+    public Double getSueldoBase() { return this.sueldoBase; }
+}
---
+++ Code/Parcial_I_V1/Nomina.java
@@ -0,0 +1,76 @@
+package Code.Parcial_I_V1;
+
+public class Nomina extends Fecha {
+    private String nombreEmpresa;
+    private String NIT;
+    private Empleado[] empleados = new Empleado[5];
+
+    public Nomina(Integer day, Integer month, Integer year) {
+        super(day, month, year);
+        this.llenarDatosEmpresa();
+        this.registrarEmpleados();
+    }
+
+    public Nomina(String day, String month, String year) {
+        super(day, month, year);
+        this.llenarDatosEmpresa();
+        this.registrarEmpleados();
+    }
+
+    public Nomina(String date) {
+        super(date);
+        this.llenarDatosEmpresa();
+        this.registrarEmpleados();
+    }
+    
+    public void llenarDatosEmpresa() {
+        System.out.println();
+    
+        System.out.format("%nIngrese el nombre de la empresa: ");
+        if (scanner.hasNext()) this.nombreEmpresa = scanner.nextLine();
+    
+        System.out.format("%nIngrese el NIT: ");
+        if (scanner.hasNext()) this.NIT = scanner.next();
+    }
+
+    public void registrarEmpleados() {
+        for (Integer i = 0; i < empleados.length; i++) {
+            // empleados[i].llenarDatos(i);
+            empleados[i] = new Empleado();
+        }
+
+    } 
+
+    public void datosEmpleado(Integer i) {
+        System.out.format("Empleado [%d]%n", i + 1);
+        System.out.format("Nombre del empleado: %s%n", empleados[i].getNombreEmpleado());
+        System.out.format("Horas trabajadas: %d%n", empleados[i].getHorasTrabajadas());
+        System.out.format("Edad: %d$n", empleados[i].getEdad());
+        System.out.format("Salario base: %f%n", empleados[i].getSalarioBase());
+        System.out.format("Sueldo base: %f%n", empleados[i].getSueldoBase());
+    }
+    
+    public void buscarEmpleado() {
+        System.out.format("Ingrese el nombre del empleado que desea buscar: ");
+        String nombreEmpleadoBuscar = this.scanner.nextLine();
+
+        Boolean empleadoEncontrado = false;
+        for (Integer i = 0; i < this.empleados.length; i++) {
+            if (nombreEmpleadoBuscar.equalsIgnoreCase(empleados[i].getNombreEmpleado())) {
+                System.out.format("Datos del empleado encontrado: %n");
+                this.datosEmpleado(i);
+                empleadoEncontrado = true;
+            }
+        }
+
+        if (!empleadoEncontrado) { System.out.format("Datos del empleado no encontrados [%s]", nombreEmpleadoBuscar); }
+    }
+
+    public void imprimirNomina() {
+        System.out.format("Nombre empresa: [%s] %n NIT: [%s] %n", this.nombreEmpresa, this.NIT);
+
+        System.out.format("Imprimiendo los datos de los empleados: %n");
+
+        for (Integer i = 0; i < this.empleados.length; i++) { this.datosEmpleado(i); }
+    }
+}
