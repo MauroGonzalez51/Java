68d85aebed314b6ca1963c682a068475bc29cc53
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-02-22 16:04:48 +0000

    Funcionando la clase Empleado > Toma de datos

---
+++ Code/Parcial_I_V2/App.java
@@ -0,0 +1,7 @@
+package Code.Parcial_I_V2;
+
+public class App {
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado();
+    }
+}
---
+++ Code/Parcial_I_V2/Empleado.java
@@ -0,0 +1,40 @@
+package Code.Parcial_I_V2;
+
+import java.util.Scanner;
+
+public class Empleado {
+    private String nombreEmpleado;
+    private Integer horasTrabajadas;
+    private Integer edadEmpleado;
+    private Double salarioBase;
+    private Double sueldoTotal;
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public Empleado() {
+        System.out.println();
+        do {
+            System.out.format("Ingrese el nombre del empleado: ");
+            this.nombreEmpleado = (this.scanner.hasNext()) ? this.scanner.nextLine() : "";
+        } while (this.nombreEmpleado.isEmpty());
+
+        do {
+            System.out.format("Ingrese la cantidad de horas trabajadas: ");
+            this.horasTrabajadas = (this.scanner.hasNextInt()) ? this.scanner.nextInt() : -1;
+        } while (this.horasTrabajadas < 0);
+
+        do {
+            System.out.format("Ingrese la edad del empleado: ");
+            this.edadEmpleado = (this.scanner.hasNextInt())? this.scanner.nextInt() : -1;
+        } while (this.edadEmpleado < 0);
+
+        do {
+            System.out.format("Ingrese el salario base del empleado: ");
+            this.salarioBase = (this.scanner.hasNextDouble())? this.scanner.nextDouble() : -1.0;
+        } while (this.salarioBase < 0.0);
+
+        this.calcularSueldoTotal();
+    }
+
+    public void calcularSueldoTotal() { this.sueldoTotal = this.horasTrabajadas * this.salarioBase; }
+}
