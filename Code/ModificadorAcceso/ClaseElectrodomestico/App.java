d5199909748c8483ecf64f64ad1cd9aceb274eb3
Mauro Gonzalez <101476062+MauroGonzalez51@users.noreply.github.com>
2023-03-08 14:32:43 +0000

    Randomizing the constructors as well

--- Code/ModificadorAcceso/ClaseElectrodomestico/App.java
+++ Code/ModificadorAcceso/ClaseElectrodomestico/App.java
@@ -26,15 +26,32 @@ public class App {
         List <String> colorValues = List.of("Blanco", "Negro", "Rojo", "Azul", "Gris", "XXX");
         List <String> efficiencyValues = List.of("A", "B", "C", "D", "E", "F", "X");
 
+        List <Integer> constructorChoice = List.of(0,1,2);
 
         // * Loop to fill the ArrayList
         for (Integer i = 0; i < cantidadDatos; i++) {
             // ! Adding a new Instance of 'Electrodomestico'
-            arrayElectrodomestico.add(new Electrodomestico(
-                randomDoubleValue(0.0,200.0), 
-                colorValues.get(randomizeValue(colorValues)), 
-                efficiencyValues.get(randomizeValue(efficiencyValues)),
-                randomDoubleValue(0.0, 200.0)));
+
+            switch (constructorChoice.get(randomizeValue(constructorChoice))) {
+                case 0: {
+                    arrayElectrodomestico.add(new Electrodomestico());
+                    break;
+                }
+
+                case 1: {
+                    arrayElectrodomestico.add(new Electrodomestico(randomDoubleValue(0.0, 200.0), randomDoubleValue(0.0, 200.0)));
+                    break;
+                }
+
+                case 2: {
+                    arrayElectrodomestico.add(new Electrodomestico(
+                        randomDoubleValue(0.0,200.0), 
+                        colorValues.get(randomizeValue(colorValues)), 
+                        efficiencyValues.get(randomizeValue(efficiencyValues)),
+                        randomDoubleValue(0.0, 200.0)));
+                    break;
+                }
+            }
 
             // * Just showing the finalPrice
             System.out.format("[ INFO ] Precio Final: %f%n", arrayElectrodomestico.get(i).getPrecioFinal());
