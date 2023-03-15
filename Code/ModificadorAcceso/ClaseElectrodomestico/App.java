7338aca59aba3237f8fc7ad904c6b22b7311653b
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-03-15 16:22:34 -0500

    Terminando el componente practico de herencia

--- Code/ModificadorAcceso/ClaseElectrodomestico/App.java
+++ Code/ModificadorAcceso/ClaseElectrodomestico/App.java
@@ -2,37 +2,42 @@ package Code.ModificadorAcceso.ClaseElectrodomestico;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class App {
-    private static <T> Integer randomizeValue(List <T> dataList) {
+    private static <T> Integer randomizeValue(List<T> dataList) {
         Integer randomIndex = (int) Math.floor(Math.random() * dataList.size());
-        return randomIndex;        
+        return randomIndex;
     }
 
     private static Boolean randomBooleanValue() {
-        List <Integer> booleanValues = List.of(0, 1);
-        return ((booleanValues.get(randomizeValue(booleanValues)) == 1) ? true : false );
+        List<Integer> booleanValues = List.of(0, 1);
+        return ((booleanValues.get(randomizeValue(booleanValues)) == 1) ? true : false);
     }
 
-    // private static Integer randomIntegerValue(Integer minValue, Integer maxValue) {
-    //    return ((int) Math.floor(Math.random() * (maxValue - minValue + 1) + minValue));
+    // private static Integer randomIntegerValue(Integer minValue, Integer maxValue)
+    // {
+    // return ((int) Math.floor(Math.random() * (maxValue - minValue + 1) +
+    // minValue));
     // }
 
     private static Double randomDoubleValue(Double minValue, Double maxValue) {
         return ((double) Math.floor(Math.random() * (maxValue - minValue + 1) + minValue));
     }
 
-    private static void dataLoop(final Integer cantidadDatos, Boolean flag) {
-        // * ArrayList that stores all the instances of 'Electrodomestico' => Preventing the IndexOutOfRangeException
-        ArrayList <Electrodomestico> arrayElectrodomestico = new ArrayList <Electrodomestico> ();
-
+    private static List <Double> dataLoop(final Integer cantidadDatos, Boolean flag) {
+        // * ArrayList that stores all the instances of 'Electrodomestico' => Preventing
+        // the IndexOutOfRangeException
+        ArrayList<Electrodomestico> arrayElectrodomestico = new ArrayList<Electrodomestico>();
 
         // ! Arrays to fill up the program with random values
-        List <String> colorValues = List.of("Blanco", "Negro", "Rojo", "Azul", "Gris", "XXX");
-        List <String> efficiencyValues = List.of("A", "B", "C", "D", "E", "F", "X");
+        List<String> colorValues = List.of("Blanco", "Negro", "Rojo", "Azul", "Gris", "XXX");
+        List<String> efficiencyValues = List.of("A", "B", "C", "D", "E", "F", "X");
 
-        List <Integer> constructorChoice = List.of(0,1,2);
+        List<Integer> constructorChoice = List.of(0, 1, 2);
+
+
+        // * [0] > Lavadora || [1] > Television || {Index}
+        Double totalLavadoras = 0.0, totalTelevision = 0.0;
 
         // * Loop to fill the ArrayList
         if (!flag) {
@@ -46,21 +51,20 @@ public class App {
                     }
 
                     case 1: {
-                        arrayElectrodomestico.add(new Electrodomestico(randomDoubleValue(0.0, 200.0), randomDoubleValue(0.0, 200.0)));
+                        arrayElectrodomestico.add(
+                                new Electrodomestico(randomDoubleValue(0.0, 200.0), randomDoubleValue(0.0, 200.0)));
                         break;
                     }
 
                     case 2: {
                         arrayElectrodomestico.add(new Electrodomestico(
-                            randomDoubleValue(0.0,200.0), 
-                            colorValues.get(randomizeValue(colorValues)), 
-                            efficiencyValues.get(randomizeValue(efficiencyValues)),
-                            randomDoubleValue(0.0, 200.0)));
+                                randomDoubleValue(0.0, 200.0),
+                                colorValues.get(randomizeValue(colorValues)),
+                                efficiencyValues.get(randomizeValue(efficiencyValues)),
+                                randomDoubleValue(0.0, 200.0)));
                         break;
                     }
                 }
-
-
 
                 // * Just showing the finalPrice
                 System.out.format("[ INFO ] Precio Final: %f%n", arrayElectrodomestico.get(i).getPrecioFinal());
@@ -69,11 +73,11 @@ public class App {
             for (Integer i = 0; i < cantidadDatos; i++) {
                 String objectInstanceOf = "";
 
-                List <Integer> constuctorChoiceBlock = List.of(1, 2);
+                List<Integer> constructorChoiceBlock = List.of(1, 2);
 
-                switch (constructorChoice.get(randomizeValue(constructorChoice))) {
+                switch (constructorChoiceBlock.get(randomizeValue(constructorChoiceBlock))) {
                     case 1: {
-                        switch (constuctorChoiceBlock.get(randomizeValue(constuctorChoiceBlock))) {
+                        switch (constructorChoiceBlock.get(randomizeValue(constructorChoiceBlock))) {
                             case 1: {
                                 arrayElectrodomestico.add(new Lavadora());
                                 break;
@@ -89,27 +93,26 @@ public class App {
                     }
 
                     case 2: {
-                        switch (constuctorChoiceBlock.get(randomizeValue(constuctorChoiceBlock))) {
+                        switch (constructorChoiceBlock.get(randomizeValue(constructorChoiceBlock))) {
                             case 1: {
                                 arrayElectrodomestico.add(new Lavadora(
-                                    randomDoubleValue(0.0, 200.0),
-                                    colorValues.get(randomizeValue(colorValues)),
-                                    efficiencyValues.get(randomizeValue(efficiencyValues)),
-                                    randomDoubleValue(0.0, 200.0),
-                                    randomDoubleValue(0.0, 200.0)
-                                ));
+                                        randomDoubleValue(0.0, 200.0),
+                                        colorValues.get(randomizeValue(colorValues)),
+                                        efficiencyValues.get(randomizeValue(efficiencyValues)),
+                                        randomDoubleValue(0.0, 200.0),
+                                        randomDoubleValue(0.0, 200.0)));
+
                                 break;
                             }
 
                             case 2: {
                                 arrayElectrodomestico.add(new Television(
-                                    randomDoubleValue(0.0, 200.0),
-                                    colorValues.get(randomizeValue(colorValues)),
-                                    efficiencyValues.get(randomizeValue(efficiencyValues)),
-                                    randomDoubleValue(0.0, 200.0),
-                                    randomDoubleValue(0.0, 200.0),
-                                    randomBooleanValue()
-                                ));
+                                        randomDoubleValue(0.0, 200.0),
+                                        colorValues.get(randomizeValue(colorValues)),
+                                        efficiencyValues.get(randomizeValue(efficiencyValues)),
+                                        randomDoubleValue(0.0, 200.0),
+                                        randomDoubleValue(0.0, 200.0),
+                                        randomBooleanValue()));
                                 break;
                             }
                         }
@@ -117,18 +120,37 @@ public class App {
                         break;
                     }
                 }
-
-                objectInstanceOf = (arrayElectrodomestico.get(i) instanceof Lavadora) ? "Lavadora" : "Television";
                 
+                if (arrayElectrodomestico.get(i) instanceof Lavadora) {
+                    totalLavadoras += arrayElectrodomestico.get(i).getPrecioFinal();
+                    objectInstanceOf = "Lavadora";
+                }
+                
+                if (arrayElectrodomestico.get(i) instanceof Television) {
+                    totalTelevision += arrayElectrodomestico.get(i).getPrecioFinal();
+                    objectInstanceOf = "Television";
+                }
+
                 // * Just showing the finalPrice
-                System.out.format("[ INFO ] Precio Final: %f [%s]%n", arrayElectrodomestico.get(i).getPrecioFinal(), objectInstanceOf);
+                System.out.format("[ INFO ] Precio Final: %f [%s]%n", arrayElectrodomestico.get(i).getPrecioFinal(),
+                        objectInstanceOf);
             }
-        } 
+        }
+        List<Double> totalCost = List.of(totalLavadoras, totalTelevision);
+        return totalCost;
     }
 
     public static void main(String[] args) {
-        // ! For testing change the value below 
+        // ! For testing change the value below
         final Integer cantidadDatos = 10;
-        dataLoop(cantidadDatos, true);
+
+        List <String> types = List.of("Lavadora", "Television");
+        List <Double> result = dataLoop(cantidadDatos, true);
+
+        System.out.println();
+        result.forEach((value) -> {
+            System.out.format("Total [%s]: %f%n", types.get(result.indexOf(value)), value);
+        });
+
     }
 }
